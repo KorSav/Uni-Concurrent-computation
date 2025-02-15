aeff9010bf93d6da9d0fb00baae0ce60c4fb5ce9
Savelii Korniiko <saveliykorniko@gmail.com>
2025-02-15 15:35:35 +0200

    fix: fix wrong calculation of ball position & size

--- lab1/ThreadBalls/Ball.cs
+++ lab1/ThreadBalls/Ball.cs
@@ -10,9 +10,9 @@ public class Ball
     private readonly Dispatcher _dispatcher;
     private readonly Ellipse _circle;
     private readonly Canvas _canvas;
-    private readonly double r = 20;
-    private double x = 0;
-    private double y = 0;
+    private readonly double r = 15;
+    private double x;
+    private double y;
     private double dx = 2;
     private double dy = 2;
 
@@ -22,14 +22,16 @@ public class Ball
         _dispatcher = dispatcher;
         var rand = new Random();
         if (rand.NextSingle() < 0.5) {
-            x = rand.NextDouble() * _canvas.ActualWidth;
+            x = r + rand.NextDouble() * (_canvas.ActualWidth - 2 * r);
+            y = r;
         }
         else {
-            y = rand.NextDouble() * _canvas.ActualHeight;
+            x = r;
+            y = r + rand.NextDouble() * (_canvas.ActualHeight - 2 * r);
         }
         _circle = new Ellipse() {
-            Width = r,
-            Height = r,
+            Width = 2 * r,
+            Height = 2 * r,
             Fill = new SolidColorBrush(Colors.Black),
         };
         _canvas.Children.Add(_circle);
@@ -39,8 +41,8 @@ public class Ball
     private void UpdatePosition()
     {
         _dispatcher.Invoke(() => {
-            Canvas.SetLeft(_circle, x);
-            Canvas.SetTop(_circle, y);
+            Canvas.SetLeft(_circle, x - r);
+            Canvas.SetTop(_circle, y - r);
         });
     }
 
@@ -48,16 +50,16 @@ public class Ball
     {
         x += dx;
         y += dy;
-        if (x < 0) {
-            x = 0;
+        if (x - r < 0) {
+            x = r;
             dx = -dx;
         }
         if (x + r >= _canvas.ActualWidth) {
             x = _canvas.ActualWidth - r;
             dx = -dx;
         }
-        if (y < 0) {
-            y = 0;
+        if (y - r < 0) {
+            y = r;
             dy = -dy;
         }
         if (y + r >= _canvas.ActualHeight) {
