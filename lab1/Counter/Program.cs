c0c4b904e4183d3d1b8d8b273601e9d94dca5d24
Savelii Korniiko <saveliykorniko@gmail.com>
2025-02-17 10:40:32 +0200

    feat: add counter project
    Runs two threads simultaneously on same counter object:
    the first one increments counter, the other - decrements
    => unpredictable result

---
+++ lab1/Counter/Program.cs
@@ -0,0 +1,32 @@
+﻿var counter = new Counter();
+var thread1 = new Thread(() => {
+    for (int i = 0; i < 100_000; i++)
+        counter.Increment();
+});
+var thread2 = new Thread(() => {
+    for (int i = 0; i < 100_000; i++)
+        counter.Decrement();
+});
+
+thread1.Start();
+thread2.Start();
+
+thread1.Join();
+thread2.Join();
+
+System.Console.WriteLine($"Counter value: {counter.Value}");
+
+class Counter
+{
+    public int Value { get; private set; } = 0;
+
+    public void Increment()
+    {
+        Value++;
+    }
+
+    public void Decrement()
+    {
+        Value--;
+    }
+}
