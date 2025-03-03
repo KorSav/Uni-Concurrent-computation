0c8ccfe98ea1aab168962b7beed618d78d08b90b
Savelii Korniiko <saveliykorniko@gmail.com>
2025-03-03 20:01:25 +0200

    feat: add chars print proj

---
+++ lab2/SymbolsSync/Program.cs
@@ -0,0 +1,14 @@
+﻿using SymbolsSync;
+
+const int charsPerRowCount = 100;
+const int rowsCount = 90;
+
+char[] chars = ['\\', '/', '|'];
+Thread[] ths = new Thread[chars.Length];
+for (int i = 0; i < chars.Length; i++){
+    CharPrinter cp = new(chars[i], charsPerRowCount, rowsCount);
+    ths[i] = new(cp.Print);
+    ths[i].Start();
+}
+
+foreach (var th in ths) th.Join();
