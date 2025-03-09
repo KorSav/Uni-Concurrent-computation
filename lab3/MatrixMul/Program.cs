6a1e0c1ded526df31ab78ac211075edb74d9d9c0
Savelii Korniiko <saveliykorniko@gmail.com>
2025-03-09 11:03:42 +0200

    feat: add matrix diff comparator

--- lab3/MatrixMul/Program.cs
+++ lab3/MatrixMul/Program.cs
@@ -10,4 +10,10 @@ for (int ir = 0; ir < matrixSize; ir++)
         matrixA[ir, ic] = random.NextDouble();
         matrixB[ir, ic] = random.NextDouble();
     }
-StripeMul.Multiply(matrixA, matrixB, 2);
+var C_stripe = StripeMul.Multiply(matrixA, matrixB, 2);
+var C_seq = SequentialMul.Multiply(matrixA, matrixB);
+System.Console.WriteLine($"Diff: {MatrixComparator.CalcMaxAbsDiff(C_seq, C_stripe)}");
+
+foreach (var v in C_stripe) System.Console.Write($"{v} ");
+System.Console.WriteLine();
+foreach (var v in C_seq) System.Console.Write($"{v} ");
