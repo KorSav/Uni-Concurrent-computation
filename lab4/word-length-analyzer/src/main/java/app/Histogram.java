5e689e8d510c2c97d6a443b0b7594c65b7998710
Savelii Korniiko <saveliykorniko@gmail.com>
2025-03-25 12:19:12 +0200

    feat: add experiment to compare methods

--- lab4/word-length-analyzer/src/main/java/app/Histogram.java
+++ lab4/word-length-analyzer/src/main/java/app/Histogram.java
@@ -3,7 +3,6 @@ package app;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.function.DoubleBinaryOperator;
 
 public class Histogram {
     private final List<Integer> wordLengthCounts = new ArrayList<>();
--- lab4/word-length-analyzer/src/main/java/app/Main.java
+++ lab4/word-length-analyzer/src/main/java/app/Main.java
@@ -5,24 +5,54 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        WordsLengthCounter wlc = new WordsLengthCounter();
         Folder folder = Folder.fromDirectory(new File(args[0]));
+        System.out.println("Warm up...");
+        doExperiment(folder, 3);
+        System.out.println("Calculating results...");
+        ExperimentResult res = doExperiment(folder, 10, true);
+        printResult(res);
+    }
 
-        long t1 = System.currentTimeMillis();
-        Histogram hist1 = wlc.getWordsLengthHistogramSequential(folder);
+    private static ExperimentResult doExperiment(Folder folder, int passesCount) {
+        return doExperiment(folder, passesCount, false);
+    }
 
-        long t2 = System.currentTimeMillis();
-        Histogram hist2 = wlc.getWordsLengthHistogramParallel(folder);
-
-        long durationSeqMs = System.currentTimeMillis() - t2;
-        long durationParMs = t2 - t1;
-
-        System.out.println("Histogram:");
-        for (int wl = 1; wl < hist1.getCount() + 1; wl++) {
-            System.out.println("%-2d - %7d  %-7d".formatted(wl, hist1.Get(wl), hist2.Get(wl)));
+    private static ExperimentResult doExperiment(Folder folder, int passesCount, boolean verbose) {
+        WordsLengthCounter wlc = new WordsLengthCounter();
+        ExperimentResult result = new ExperimentResult();
+        long tpSum = 0, tsSum = 0;
+        for (int i = 0; i < passesCount; i++) {
+            if (verbose)
+                System.out.print("\rDoing experiment %2d/%-2d".formatted(i + 1, passesCount));
+            long t1 = System.currentTimeMillis();
+            result.histSeq = wlc.getWordsLengthHistogramSequential(folder);
+            long t2 = System.currentTimeMillis();
+            result.histPar = wlc.getWordsLengthHistogramParallel(folder);
+            tpSum += System.currentTimeMillis() - t2;
+            tsSum += t2 - t1;
         }
-        System.out.println("Mean: %.3f".formatted(hist1.CalculateMean()));
-        System.out.println("Variance: %.3f".formatted(hist1.CalculateVariance()));
-        System.out.println("Seq: %d ms\nParallel: %d ms".formatted(durationParMs, durationSeqMs));
+        if (verbose)
+            System.out.println();
+        result.timePar = tpSum / passesCount;
+        result.timeSeq = tsSum / passesCount;
+        result.procCount = Runtime.getRuntime().availableProcessors();
+        return result;
+    }
+
+    private static void printResult(ExperimentResult result) {
+        System.out.println("Histogram:");
+        System.out.println("Word lenght | Sequential count | Parallel count");
+        for (int wl = 1; wl < result.histSeq.getCount() + 1; wl++) {
+            System.out.println("%-2d - %7d  %-7d"
+                    .formatted(wl, result.histSeq.Get(wl), result.histPar.Get(wl)));
+        }
+        System.out.println("Mean: %.3f".formatted(result.histSeq.CalculateMean()));
+        System.out.println("Variance: %.3f".formatted(result.histSeq.CalculateVariance()));
+        System.out.println("\nMethods comparison:");
+        System.out.println("Time sequential: %d ms".formatted(result.timeSeq));
+        System.out.println("Time parallel  : %d ms".formatted(result.timePar));
+        System.out.println("Speedup: %f".formatted(result.GetSpeedup()));
+        System.out.println("Efficiency: %f".formatted(result.GetEfficiency()));
+        System.out.println("Cost: %d".formatted(result.GetCost()));
     }
 }
