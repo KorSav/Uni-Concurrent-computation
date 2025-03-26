20008b2587bc898b632435d6148a7d0c81937fd6
Savelii Korniiko <saveliykorniko@gmail.com>
2025-03-26 15:32:42 +0200

    feat: make experiments to compare threads and fork/join

--- lab4/matrix-mul-fork-join/src/main/java/app/Main.java
+++ lab4/matrix-mul-fork-join/src/main/java/app/Main.java
@@ -9,17 +9,45 @@ import app.Utils.Result;
 
 public class Main {
     public static void main(String[] args) {
-        MatrixPair mp = MatrixUtils.generateRandomMatrices(500);
-        Result resStripe = StripeMul.multiply(mp.getMatrixA(), mp.getMatrixB(), 12);
-        Result resSeq = SequentialMul.multiply(mp.getMatrixA(), mp.getMatrixB());
-        Result resForkJoin = app.ForkJoinImpl.StripeMul.multiply(mp.getMatrixA(), mp.getMatrixB());
-        double diff = MatrixComparator.calcMaxAbsDiff(
-                resStripe.getResultMatrix(),
-                resSeq.getResultMatrix());
-        System.out.println("Stripe threads diff: %e".formatted(diff));
-        diff = MatrixComparator.calcMaxAbsDiff(
-                resForkJoin.getResultMatrix(),
-                resSeq.getResultMatrix());
-        System.out.println("Stripe fork/join diff: %e".formatted(diff));
+        int[] matrixSizes = new int[] { 500, 1000, 1500, 2000, 2500, 3000 };
+        System.out.println("Warm up...");
+        doExperiment(matrixSizes[1], 3, false);
+        System.out.println("Matrix size | T_seq, s | T_ts, s | T_fj, s | S_ts | S_fj");
+        for (int ms : matrixSizes) {
+            ExperimentResult er = doExperiment(ms, 3, true);
+            System.out.println("\r%12d|%10.3f|%9.3f|%9.3f|%6.3f|%6.3f - %.3e | %.3e"
+                    .formatted(ms, er.GetTimeSeqSecs(),
+                            er.GetTimeThreadsSecs(), er.GetTimeForkJoinSecs(),
+                            er.GetSpeedupThreads(), er.GetSpeedupForkJoin(),
+                            er.diffThreads, er.diffForkJoin));
+        }
+    }
+
+    private static ExperimentResult doExperiment(int matrixSize, int passesCount, boolean verbose) {
+        ExperimentResult result = new ExperimentResult();
+        int procCount = Runtime.getRuntime().availableProcessors();
+        Result resForkJoin, resSeq, resStripe;
+        for (int i = 0; i < passesCount; i++) {
+            if (verbose)
+                System.out.print("\r%12d| pass %2d/%-2d"
+                        .formatted(matrixSize, i + 1, passesCount));
+            MatrixPair mp = MatrixUtils.generateRandomMatrices(matrixSize);
+            resStripe = StripeMul.multiply(mp.getMatrixA(), mp.getMatrixB(), procCount);
+            result.timeThreadsNanos += resStripe.getElapsedTimeNano();
+            resForkJoin = app.ForkJoinImpl.StripeMul.multiply(mp.getMatrixA(), mp.getMatrixB());
+            result.timeForkJoinNanos += resForkJoin.getElapsedTimeNano();
+            resSeq = SequentialMul.multiply(mp.getMatrixA(), mp.getMatrixB());
+            result.timeSeqNanos += resSeq.getElapsedTimeNano();
+            result.diffThreads = MatrixComparator.calcMaxAbsDiff(
+                    resStripe.getResultMatrix(),
+                    resSeq.getResultMatrix());
+            result.diffForkJoin = MatrixComparator.calcMaxAbsDiff(
+                    resForkJoin.getResultMatrix(),
+                    resSeq.getResultMatrix());
+        }
+        result.timeForkJoinNanos /= passesCount;
+        result.timeThreadsNanos /= passesCount;
+        result.timeSeqNanos /= passesCount;
+        return result;
     }
 }
